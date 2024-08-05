88c8767728f6050982fa24e71d5d94285b34b35b
Souhail-Segh <souhail.segh.ds@gmail.com>
2024-08-05 23:49:12 +0000

    Task 0: correction

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,8 +9,6 @@ import time
 async def wait_random(max_delay=10):
     '''Wait a random time and return it.
     '''
-    s = time.perf_counter()
-    r = random.uniform(0, max_delay)
+    run_time = random.uniform(0, max_delay)
     await asyncio.sleep(r)
-    run_time = time.perf_counter() - s
     return run_time
