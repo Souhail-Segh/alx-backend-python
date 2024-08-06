8e9388cb89583ac1c3cf4dc6f36eb5c5ac016841
Souhail-Segh <souhail.segh.ds@gmail.com>
2024-08-06 01:44:30 +0000

    Task 2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''Calculate the average executin time of co-routines.
+'''
+import asyncio
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    '''Measure average time of asyncio.sleep.
+    '''
+    start_time = time.time()
+    asyncio.run( wait_n(n, max_delay))
+    stop_time = time.time()
+    return (stop_time - start_time) / n
